90d6f4dc55a9b2bf15050d778c66e1b90435797f
Adriel Hakiem <78272170+adrielhakiiem@users.noreply.github.com>
2022-12-28 03:34:52 +0800

    Block Letters
    ASCII art is a graphic design technique that uses computers for presentation and consists of pictures pieced together from individual characters.
    
    Write a Python program called initials.py that displays the initials of your name in block letters as shown and dip your toes into ASCII art.

---
+++ Hello-World/block_letters.py
@@ -0,0 +1,23 @@
+firstinitial = """
+SSS   
+S   S  
+S      
+ SSS   
+    S  
+S   S  
+ SSS
+"""
+
+secondinitial = """ 
+ L
+ L
+ L
+ L
+ L
+ L
+ LLLLL
+"""
+print(firstinitial + secondinitial)
+
+
+
