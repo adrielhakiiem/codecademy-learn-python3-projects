38a32b83ff7c6a790e1510027a8e7fa70ae6e0ef
Adriel Hakiem <78272170+adrielhakiiem@users.noreply.github.com>
2023-01-18 18:30:15 +0800

    Carly's Clippers info
    You are the Data Analyst at Carly’s Clippers, the newest hair salon on the block. Your job is to go through the lists of data that have been collected in the past couple of weeks. You will be calculating some important metrics that Carly can use to plan out the operation of the business for the rest of the month.
    
    You have been provided with three lists:
    
    hairstyles: the names of the cuts offered at Carly’s Clippers.
    prices: the price of each hairstyle in the hairstyles list.
    last_week: the number of purchases for each hairstyle type in the last week.
    Each index in hairstyles corresponds to an associated index in prices and last_week.
    
    For example, The hairstyle "bouffant" has an associated price of 30 from the prices list, and was purchased 2 times in the last week as shown in the last_week list. Each of these elements are in the first index of their respective lists.

--- Loops/Carlys_Clippers.py
+++ Loops/Carlys_Clippers.py
@@ -4,34 +4,40 @@ prices = [30, 25, 40, 20, 20, 35, 50, 35]
 
 last_week = [2, 3, 5, 8, 4, 4, 6, 2]
 
+# sum up ALL the prices of haircuts 
 total_price = 0
 
+# loop through prices list and add each price to the variable total_price
 for price in prices:
-  total_price += price 
+    total_price += price # runs 
 
-print("This is the total price: " + str(total_price))
-print("\n")
+# find average price 
+average_price = total_price / len(prices) # runs 
 
-average_price = total_price / len(prices)
 print("Average Haircut Price: " + str(average_price))
-print("\n")
 
+# cut all prices by 5 dollars and add it into a new list called new_prices using list comprehension 
 new_prices = [price - 5 for price in prices]
-print("This is the new prices for each haircut: " + str(new_prices))
-print("\n")
 
+print(new_prices) # runs 
+
+# checking how much revenue
 total_revenue = 0
 
-for i in range(len(hairstyles)):
-  total_revenue += (prices[i] * last_week[i])
+for i in range(0, len(hairstyles)):
+    # product of prices and last_week and add it to total_revenue 
+    total_revenue += prices[i] * last_week[i]
 
-print("Total Revenue: " + str(total_revenue)) 
-print("\n")
+print("Total Revenue: " + str(total_revenue)) # runs 
 
-average_daily_revenue = total_revenue / 7
-print("Average Daily Revenue: " + str(average_daily_revenue))
-print("\n")
+# find the average daily revenue 
+average_daily_revenue = total_revenue / 7 
 
-cuts_under_30 = [hairstyles[i] for i in  range(len(hairstyles[i])) if new_prices[i] < 30]
+print("The Average Daily Revenue is: " + str(average_daily_revenue)) # runs 
 
-print("These are the cuts under 30: "+ str(cuts_under_30))
+# create a list of cuts under the price of 30 to advertise shop using list comprehension (display name of hairstyles instead of the price) 
+cuts_under_30 = [] 
+
+cuts_under_30 = [hairstyles[i] for i in range(len(hairstyles[i])) if new_prices[i] < 30]
+
+print("These are the haircuts that are under $30: " + str(cuts_under_30))
