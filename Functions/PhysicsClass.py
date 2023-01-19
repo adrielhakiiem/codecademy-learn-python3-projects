1fc0e9b62f90c5343a6839f797dce9f5b5ab3e01
Adriel Hakiem <78272170+adrielhakiiem@users.noreply.github.com>
2023-01-19 17:49:41 +0800

    Physics Class Info
    You are a physics teacher preparing for the upcoming semester. You want to provide your students with some functions that will help them calculate some fundamental physical properties.

--- Functions/PhysicsClass.py
+++ Functions/PhysicsClass.py
@@ -1,5 +1,6 @@
 # Getting Ready for Physics Class! 
 
+# Uncomment this when you reach the "Use the Force" section
 train_mass = 22680
 train_acceleration = 10
 train_distance = 100
@@ -7,48 +8,45 @@ bomb_mass = 1
 
 
 # Write your code below: 
+
+# f_to_c, a function that takes fahrenheit as argument and converts it to celcius 
 def f_to_c(f_temp):
-  c_temp = (f_temp - 32) * 5/9
-  return c_temp
+    # celcius = (fahrenheit - 32) * 5/9
+    c_temp = (f_temp - 32) * 5/9
+    return c_temp
 
-f100_in_celcius = f_to_c(100)
-print(round(f100_in_celcius, 2))
- 
-# ------------------------------------- #
+f100_in_celcius = f_to_c(100) # runs 
 
+# c_to_f, a function that takes celcius as argument and converts it to fahrenheit 
 def c_to_f(c_temp):
-  f_temp = c_temp * (9/5) + 32
-  return f_temp
+    f_temp = c_temp * (9/5) + 32 
+    return f_temp
 
-c0_in_fahrenheit = c_to_f(0)
-print(c0_in_fahrenheit)
+c0_in_fahrenheit = c_to_f(0) # runs 
 
-# ------------------------------------- #
-
+# get_force, a function that takes a product of mass and acceleration
 def get_force(mass, acceleration):
-  return mass * acceleration
+    return mass * acceleration
 
-train_force = get_force(train_mass, train_acceleration)
-# print(train_force)
+train_force = get_force(train_mass, train_acceleration) # output = 226800 
 
-# ------------------------------------- #
+print("The GE train supplies " + str(train_force) + " Newtons of force.") # runs 
 
-print("The GE train supplies " + str(train_force) + " Newtons of force.")
-
-# ------------------------------------- #
-
-c = 3*10**8
-
+# get_energy, a function that takes a product of mass and c squared 
+c = 3*10**8 
 def get_energy(mass, c):
-  return mass * (c**2)
+    return mass * (c**2)
 
-bomb_energy = get_energy(bomb_mass, c)
-print("A 1kg bomb supplies " + str(bomb_energy) + "Joules.")
+bomb_energy = get_energy(bomb_mass, c) 
 
-# ------------------------------------- #
+print("A 1kg bomb supplies " + str(bomb_energy) + " Joules.") # runs 
 
+# get_work, a function that that takes mass, acceleration, distance
+
+# work = force(get_force) * distance 
 def get_work(mass, acceleration, distance):
-  return get_force(mass, acceleration) * distance
+    return get_force(mass, acceleration) * distance
 
 train_work = get_work(train_mass, train_acceleration, train_distance)
+
 print("The GE train does " + str(train_work) + " Joules of work over " + str(train_distance) + " meters.")
